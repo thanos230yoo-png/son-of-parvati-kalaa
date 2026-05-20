2088fb99bac0918c159ba717ba2102f936eb615b
thanos230yoo-png <thanos230yoo@gmail.com>
2026-05-20 22:05:00 +0530

    real image upload added

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,31 +5,48 @@ import { supabase } from "../lib/supabase";
 export default function AdminPage() {
 
   const [title, setTitle] = useState("");
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState<any>(null);
   const [category, setCategory] = useState("");
 
   async function uploadPost() {
 
-    const { error } = await supabase.from("posts").insert([
+  if (!image) {
+    alert("Choose image first");
+    return;
+  }
+
+  const fileName = Date.now() + "-" + image.name;
+
+  const { error: uploadError } = await supabase.storage
+    .from("kalaa")
+    .upload(fileName, image);
+
+  if (uploadError) {
+    console.log(uploadError);
+    alert("Image upload failed");
+    return;
+  }
+
+  const imageUrl =
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/kalaa/${fileName}`;
+
+  const { error } = await supabase
+    .from("posts")
+    .insert([
       {
         title,
-        image,
+        image: imageUrl,
         category,
       },
     ]);
 
-    if (error) {
-      alert("Upload failed ");
-      console.log(error);
-    } else {
-      alert("Kalaa uploaded ");
-
-      setTitle("");
-      setImage("");
-      setCategory("");
-    }
+  if (error) {
+    console.log(error);
+    alert("Database upload failed");
+  } else {
+    alert("Kalaa uploaded 🔥");
   }
-
+}
   return (
     <main className="min-h-screen bg-black text-white flex flex-col items-center p-10">
 
@@ -40,11 +57,9 @@ export default function AdminPage() {
       <div className="flex flex-col gap-6 w-full max-w-xl">
 
         <input
-          type="text"
-          placeholder="Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="bg-zinc-900 p-4 rounded-xl"
+          type="file"
+          onChange={(e) => setImage(e.target.files![0])}
+        className="w-full bg-zinc-900 p-4 rounded-xl"
         />
 
         <input
