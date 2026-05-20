666cdb41adfcbe7369c08c61bf5ebe809fda6e10
thanos230yoo-png <thanos230yoo@gmail.com>
2026-05-20 20:20:01 +0530

    updated search system

---
+++ app/durgamaa/page.tsx
@@ -0,0 +1,7 @@
+export default function DurgaMaa() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center text-5xl">
+      Durga Maa Kalaa
+    </div>
+  );
+}
