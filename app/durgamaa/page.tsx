6715a637e407dfd0da6b58fecc67efd2f271b26c
thanos230yoo-png <thanos230yoo@gmail.com>
2026-05-20 20:27:55 +0530

    updated search system

--- app/durgamaa/page.tsx
+++ app/durgamaa/page.tsx
@@ -1,7 +1,17 @@
 export default function DurgaMaa() {
   return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center text-5xl">
-      Durga Maa Kalaa
-    </div>
+    <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
+
+      <img
+        src="/durgamaa.jpg"
+        alt="Durga Maa"
+        className="w-[500px] rounded-3xl border border-red-500 shadow-2xl"
+      />
+
+      <h1 className="text-5xl font-bold mt-10 text-red-500">
+        Durga Maa Kalaa
+      </h1>
+
+    </main>
   );
 }
