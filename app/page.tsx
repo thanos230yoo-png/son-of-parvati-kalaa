274b58a47611fc9892df3c0fcd1dae2b107dc6cc
thanos230yoo-png <thanos230yoo@gmail.com>
2026-05-23 22:08:03 +0530

    added admin dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -345,7 +345,13 @@ export default function Home() {
                 <h2 className="text-2xl font-bold">
                   {post.title}
                 </h2>
-                
+{
+  post.category === "Durga"
+    ? "Durga Maa"
+    : post.category === "Kali"
+    ? "Kali Maa"
+    : post.category
+}
 
                 <div className="flex gap-4 mt-4 flex-wrap">
 
