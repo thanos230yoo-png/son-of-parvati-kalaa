9b4e7103807f0dc099f37db6a40f8fd072455d56
thanos230yoo-png <thanos230yoo@gmail.com>
2026-05-24 00:52:24 +0530

    lets see

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,36 +1,26 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
 
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
-
     {
       url: "https://son-of-parvati-kalaa.vercel.app",
       lastModified: new Date(),
     },
-
     {
       url: "https://son-of-parvati-kalaa.vercel.app/kalimaa",
       lastModified: new Date(),
     },
-
     {
       url: "https://son-of-parvati-kalaa.vercel.app/durgamaa",
       lastModified: new Date(),
     },
-
     {
       url: "https://son-of-parvati-kalaa.vercel.app/shiva",
       lastModified: new Date(),
     },
-
     {
       url: "https://son-of-parvati-kalaa.vercel.app/krishna",
       lastModified: new Date(),
     },
-
-    {
-      url: "https://son-of-parvati-kalaa.vercel.app/admin",
-      lastModified: new Date(),
-    },
-
   ];
 }
